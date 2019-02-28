f85c328d86df89b86296a1f041f8d7dc712de3b8
Puria Nafisi Azizi <puria@dyne.org>
2019-02-28 20:00:19 +0100

    :white_check_mark: Updating to latest contracts and tests

--- test/run.sh
+++ test/run.sh
@@ -6,13 +6,15 @@ set -o pipefail
 # set -x
 # https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 
-/code/zenroom/src/zenroom-static /src/01-CITIZEN-request-keypair.zencode > keypair.keys 2>/dev/null
-/code/zenroom/src/zenroom-static -k keypair.keys /src/02-CITIZEN-request-blind-signature.zencode > blind_signature.req  2>/dev/null
-/code/zenroom/src/zenroom-static /src/03-CREDENTIAL_ISSUER-keypair.zencode > ci_keypair.keys  2>/dev/null
-/code/zenroom/src/zenroom-static -k ci_keypair.keys /src/04-CREDENTIAL_ISSUER-public-verify-keypair.zencode > ci_verify_keypair.keys  2>/dev/null
-/code/zenroom/src/zenroom-static -k ci_keypair.keys -a blind_signature.req /src/05-CREDENTIAL_ISSUER-credential-blind-signature.zencode > ci_signed_credential.json  2>/dev/null
-/code/zenroom/src/zenroom-static -k keypair.keys -a ci_signed_credential.json /src/06-CITIZEN-credential-blind-signature.zencode > credential.json  2>/dev/null
-/code/zenroom/src/zenroom-static -k ci_verify_keypair.keys -a credential.json /src/07-CITIZEN-blind-proof-credential.zencode > blindproof_credential.json  2>/dev/null
-/code/zenroom/src/zenroom-static -k ci_verify_keypair.keys -a blindproof_credential.json /src/08-VERIFIER-verify-blind-proof-credential.zencode  2>/dev/null
+/code/zenroom/src/zenroom-static  01-CITIZEN-credential-keygen.zencode > keypair.keys  2>/dev/null
+/code/zenroom/src/zenroom-static  -k keypair.keys 02-CITIZEN-credential-request.zencode > blind_signature.req  2>/dev/null
+/code/zenroom/src/zenroom-static  03-CREDENTIAL_ISSUER-keygen.zencode > ci_keypair.keys  2>/dev/null
+/code/zenroom/src/zenroom-static  -k ci_keypair.keys 04-CREDENTIAL_ISSUER-publish-verifier.zencode > ci_verify_keypair.keys  2>/dev/null
+/code/zenroom/src/zenroom-static  -k ci_keypair.keys -a blind_signature.req 05-CREDENTIAL_ISSUER-credential-sign.zencode > ci_signed_credential.json  2>/dev/null
+/code/zenroom/src/zenroom-static  -k keypair.keys -a ci_signed_credential.json 06-CITIZEN-aggregate-credential-signature.zencode > credential.json  2>/dev/null
+/code/zenroom/src/zenroom-static  -k credential.json -a ci_verify_keypair.keys 07-CITIZEN-prove-credential.zencode > blindproof_credential.json  2>/dev/null
+/code/zenroom/src/zenroom-static  -a ci_verify_keypair.keys -k blindproof_credential.json 08-VERIFIER-verify-credential.zencode  2>/dev/null
+
+
 
 exit $?
